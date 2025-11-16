8f0258777563d43cc987d7f79add35cb450d1632
rohithshan23 <rohithshan23@gmail.com>
2025-11-16 17:52:44 +0530

    main.py updated for cloud build

--- main.py
+++ main.py
@@ -2,34 +2,27 @@ import os
 import requests
 from datetime import datetime
 from dotenv import load_dotenv
+from flask import jsonify
 
 load_dotenv()
 
 API_KEY = os.getenv("EXCHANGERATE_API_KEY")
 BASE_URL = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/USD"
 
-# Add currencies you want to track
-CURRENCY_PAIRS = [
-    "INR",
-    "EUR",
-    "USD",
-    "JPY", 
-    "GBP"
-]
+CURRENCY_PAIRS = ["INR", "EUR", "USD", "JPY", "GBP"]
 
-def fetch_forex_rates():
+def fetch_forex_rates(request):
     try:
         response = requests.get(BASE_URL)
         data = response.json()
 
         if data.get("result") != "success":
-            raise Exception(f"API Error: {data.get('error-type')}")
+            return jsonify({"error": data.get("error-type")}), 500
 
         rates = data["conversion_rates"]
+        timestamp = datetime.utcnow().isoformat()
 
         forex_data = []
-        # timestamp = data["time_last_update_utc"]
-        timestamp = datetime.utcnow().isoformat()
 
         for currency in CURRENCY_PAIRS:
             forex_data.append({
@@ -39,13 +32,7 @@ def fetch_forex_rates():
                 "timestamp": timestamp
             })
 
-        return forex_data
+        return jsonify(forex_data)
 
     except Exception as e:
-        print("Error fetching forex rates:", e)
-        return None
-
-
-if __name__ == "__main__":
-    result = fetch_forex_rates()
-    print(result)
+        return jsonify({"error": str(e)}), 500
